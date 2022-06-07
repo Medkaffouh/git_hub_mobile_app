a306f1d1f04fb156fa6cf7f390afc2c458f94fb8
Medkaffouh <kaffouhmohamed@gmail.com>
2022-06-07 23:52:27 +0100

    create repo page

--- lib/pages/repositories/home.page.dart
+++ lib/pages/repositories/home.page.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class GitRepositoriesPage extends StatelessWidget {
+class GitRepositoriesPage extends StatefulWidget {
   String? login;
   GitRepositoriesPage({Key? key, this.login}) : super(key: key);
+
+  @override
+  State<GitRepositoriesPage> createState() => _GitRepositoriesPageState();
+}
+
+class _GitRepositoriesPageState extends State<GitRepositoriesPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadRepositories();
+  }
+  void loadRepositories(){
+    String url = "https://api.github.com/users/mohamedYoussfi/repos";
+    http.get(Uri.parse(url));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Repositories $login'),),
+      appBar: AppBar(title: Text('Repositories ${widget.login}'),),
       body:  Center(
-        child: Text('Repositories $login'),
+        child: Text('Repositories ${widget.login}'),
       ),
     );
   }
